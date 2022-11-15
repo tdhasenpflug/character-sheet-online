4ba7feb04ec468b0470e8fddf4f0fdd06d85cb27
Taylor Hasenpflug <tdhasenpflug@gmail.com>
2022-11-14 19:07:33 -0800

    change text color, writing font, formatting changes, saving, etc

--- sheet.js
+++ sheet.js
@@ -6,9 +6,8 @@ window.addEventListener('load', () => {
     var save;
 
     var saveRequired = false;
-    sheet.addEventListener('keyup', (ev) => {
-        saveRequired = true;
-    })
+    sheet.addEventListener('keyup', () => { saveRequired = true })
+    sheet.addEventListener('click', () => { saveRequired = true })
 
     const qs = new URLSearchParams(window.location.search)
     if (qs.has("cid")) {
@@ -19,7 +18,11 @@ window.addEventListener('load', () => {
             if (name != "uid") {
                 let el = document.getElementsByName(name)
                 if (el.length > 0) {
-                    el[0].setAttribute("value", ch[name])
+                    if (el[0].getAttribute("type") == "checkbox") {
+                        el[0].setAttribute("checked", "checked")
+                    } else {
+                        el[0].setAttribute("value", ch[name])
+                    }
                 }
             }
         }
@@ -43,6 +46,31 @@ window.addEventListener('load', () => {
         }
 
         saveRequired = false
-    }, 15000)
+        console.log("Complete.")
+    }, 5000)
+
+
+
+    const penColor = document.getElementById("tool_pen_color")
+    const writingFont = document.getElementById("tool_writing_font")
+
+    penColor.addEventListener('change', (ev) => {
+        sheet.classList.remove("pc_blue")
+        sheet.classList.remove("pc_green")
+        sheet.classList.remove("pc_red")
+
+        if (penColor.value.length > 0) {
+            sheet.classList.add(penColor.value)
+        }
+    })
+    writingFont.addEventListener('change', (ev) => {
+        sheet.classList.remove("wf_mono")
+        sheet.classList.remove("wf_serif")
+        sheet.classList.remove("wf_sans")
+
+        if (writingFont.value.length > 0) {
+            sheet.classList.add(writingFont.value)
+        }
+    })
 
 })
